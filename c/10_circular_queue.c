fd6e1e59dc1c4bd055247765650e02bedf2293b8
Hitalo Souza <enghitalo@gmail.com>
2024-10-28 17:12:45 -0400

    make it adaptive

--- c/10_circular_queue.c
+++ c/10_circular_queue.c
@@ -1,17 +1,16 @@
 // Circular Queue with Mutex and Atomic Operations
 
 /*
-clear && wrk -t16 -c512 -d30s http://127.0.0.1:8080
+clear && wrk -t16 -c512 -d60s http://127.0.0.1:8080
 
-Running 30s test @ http://127.0.0.1:8080
+Running 1m test @ http://127.0.0.1:8080
   16 threads and 512 connections
   Thread Stats   Avg      Stdev     Max   +/- Stdev
-    Latency     2.71ms    1.16ms  23.37ms   72.20%
-    Req/Sec     8.72k   609.28    10.68k    70.10%
-  4169586 requests in 30.10s, 0.00B read
-  Socket errors: connect 0, read 4169586, write 0, timeout 0
-Requests/sec: 138528.00
-Transfer/sec:       0.00B
+    Latency     2.76ms    1.15ms  17.82ms   71.10%
+    Req/Sec     8.53k   612.66    10.46k    68.76%
+  8154737 requests in 1.00m, 0.90GB read
+Requests/sec: 135678.87
+Transfer/sec:     15.27MB
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,32 +21,39 @@ Transfer/sec:       0.00B
 #include <stdatomic.h>
 
 #define PORT 8080
-#define BACKLOG 512
-#define NUM_WORKERS 16
-
-const char *response = "HTTP/1.1 200 OK\r\n"
-                       "Date: Wed, 23 Oct 2024 12:00:00 GMT\r\n"
-                       "Content-Type: application/json\r\n"
-                       "Content-Length: 27\r\n"
-                       "Connection: keep-alive\r\n\r\n"
-                       "{\r\n  \"message\": \"Hello, world!\"\r\n}";
+#define INITIAL_THREAD_POOL_SIZE 16
+#define MAX_THREAD_POOL_SIZE 16
+#define MAX_CONNECTION_SIZE 2048
+#define MAX_WAITING_QUEUE_SIZE 1024
+#define BACKLOG 1024
+#define BUFFER_SIZE 2048
+#define RESPONSE_BODY "{\"message\": \"Hello, world!\"}"
+#define RESPONSE_BODY_LENGTH (sizeof(RESPONSE_BODY) - 1)
 
 int server_fd;
-pthread_t thread_ids[NUM_WORKERS];
+pthread_t *thread_ids;
 pthread_mutex_t lock;
-int client_queue[BACKLOG];
+int client_queue[MAX_WAITING_QUEUE_SIZE];
 int queue_front = 0, queue_rear = 0;
 atomic_int queue_size = 0;  // Use atomic int for queue size
 atomic_int has_clients = 0; // Flag to indicate if there are clients
+atomic_int current_thread_pool_size = INITIAL_THREAD_POOL_SIZE;
 
 // Function to enqueue a client connection
 void enqueue_client(int client_fd)
 {
     pthread_mutex_lock(&lock);
-    client_queue[queue_rear] = client_fd;
-    queue_rear = (queue_rear + 1) % BACKLOG;
-    atomic_fetch_add(&queue_size, 1);
-    atomic_store(&has_clients, 1); // Set flag indicating clients are present
+    if (atomic_load(&queue_size) < MAX_WAITING_QUEUE_SIZE)
+    {
+        client_queue[queue_rear] = client_fd;
+        queue_rear = (queue_rear + 1) % MAX_WAITING_QUEUE_SIZE;
+        atomic_fetch_add(&queue_size, 1);
+        atomic_store(&has_clients, 1); // Set flag indicating clients are present
+    }
+    else
+    {
+        close(client_fd); // Reject connection if queue is full
+    }
     pthread_mutex_unlock(&lock);
 }
 
@@ -62,7 +68,7 @@ int dequeue_client()
         if (atomic_load(&queue_size) > 0)
         {
             client_fd = client_queue[queue_front];
-            queue_front = (queue_front + 1) % BACKLOG;
+            queue_front = (queue_front + 1) % MAX_WAITING_QUEUE_SIZE;
             atomic_fetch_sub(&queue_size, 1);
             if (atomic_load(&queue_size) == 0)
             {
@@ -72,10 +78,8 @@ int dequeue_client()
             break;
         }
         pthread_mutex_unlock(&lock);
-        // Optionally add a short sleep to reduce busy-waiting
-        usleep(100); // Sleep for 100 microseconds
+        usleep(100); // Reduce busy-waiting
     }
-
     return client_fd;
 }
 
@@ -85,16 +89,56 @@ void *worker_thread(void *arg)
     while (1)
     {
         int client_fd = dequeue_client();
-        char buffer[1024] = {0};
+        char buffer[BUFFER_SIZE] = {0};
 
         // Read request
-        read(client_fd, buffer, 1024);
+        read(client_fd, buffer, BUFFER_SIZE);
 
-        // Send response
-        write(client_fd, response, strlen(response));
+        // Write and send response
+        char response[BUFFER_SIZE];
+        int response_length = snprintf(response, sizeof(response),
+                                       "HTTP/1.1 200 OK\r\n"
+                                       "Content-Type: application/json\r\n"
+                                       "Content-Length: %zu\r\n"
+                                       "Connection: close\r\n\r\n"
+                                       "%s",
+                                       RESPONSE_BODY_LENGTH, RESPONSE_BODY);
 
-        // Close connection
+        write(client_fd, response, response_length);
         close(client_fd);
+    }
+    return NULL;
+}
+
+void adjust_thread_pool_size()
+{
+    int queue_len = atomic_load(&queue_size);
+    int pool_size = atomic_load(&current_thread_pool_size);
+
+    if (queue_len > (MAX_WAITING_QUEUE_SIZE / 2) && pool_size < MAX_THREAD_POOL_SIZE)
+    {
+        int new_size = pool_size + 2;
+        new_size = new_size > MAX_THREAD_POOL_SIZE ? MAX_THREAD_POOL_SIZE : new_size;
+        for (int i = pool_size; i < new_size; i++)
+        {
+            pthread_create(&thread_ids[i], NULL, worker_thread, NULL);
+        }
+        atomic_store(&current_thread_pool_size, new_size);
+    }
+    else if (queue_len < (MAX_WAITING_QUEUE_SIZE / 4) && pool_size > INITIAL_THREAD_POOL_SIZE)
+    {
+        int new_size = pool_size - 2;
+        new_size = new_size < INITIAL_THREAD_POOL_SIZE ? INITIAL_THREAD_POOL_SIZE : new_size;
+        atomic_store(&current_thread_pool_size, new_size);
+    }
+}
+
+void *monitor_thread(void *arg)
+{
+    while (1)
+    {
+        adjust_thread_pool_size();
+        sleep(1);
     }
     return NULL;
 }
@@ -103,6 +147,8 @@ int main()
 {
     struct sockaddr_in address;
     int addrlen = sizeof(address);
+    thread_ids = malloc(MAX_THREAD_POOL_SIZE * sizeof(pthread_t));
+    pthread_t monitor_tid;
 
     // Create server socket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -144,10 +190,11 @@ int main()
     pthread_mutex_init(&lock, NULL);
 
     // Create worker threads
-    for (int i = 0; i < NUM_WORKERS; i++)
+    for (int i = 0; i < INITIAL_THREAD_POOL_SIZE; i++)
     {
         pthread_create(&thread_ids[i], NULL, worker_thread, NULL);
     }
+    pthread_create(&monitor_tid, NULL, monitor_thread, NULL);
 
     while (1)
     {
@@ -166,6 +213,7 @@ int main()
     // Cleanup
     pthread_mutex_destroy(&lock);
     close(server_fd);
+    free(thread_ids);
 
     return 0;
 }
