39e2620520bd860a4ac84c80cb0a89d9322a8708
Hitalo Souza <enghitalo@gmail.com>
2024-11-06 09:04:14 -0400

    style: fmt -w and use C.errno

--- c/11_non_blocking_mode.v
+++ c/11_non_blocking_mode.v
@@ -160,7 +160,7 @@ fn create_server_socket() int {
 fn add_fd_to_epoll(epoll_fd int, fd int, events u32) int {
 	ev := EpollEvent{
 		events: events
-		data: Epoll_data_t{
+		data:   Epoll_data_t{
 			fd: fd
 		}
 	}
@@ -176,33 +176,33 @@ fn remove_fd_from_epoll(epoll_fd int, fd int) {
 	C.epoll_ctl(epoll_fd, C.EPOLL_CTL_DEL, fd, (unsafe { nil }))
 }
 
-fn handle_accept(server_fd int, epoll_fd int,lock_flag &sync.Mutex) {
+fn handle_accept(server_fd int, epoll_fd int, lock_flag &sync.Mutex) {
 	for {
-		client_fd := C.accept(server_fd, (unsafe { nil }), (unsafe { nil }))
+		client_fd := C.accept(server_fd, unsafe { nil }, unsafe { nil })
 		if client_fd < 0 {
-			if (error_code()) == 11 || (error_code()) == 11 {
-				break
+			// Check for EAGAIN or EWOULDBLOCK, usually represented by errno 11.
+			if C.errno == C.EAGAIN || C.errno == C.EWOULDBLOCK {
+				break // No more incoming connections; exit loop.
 			}
-			eprintln('Accept failed')
+			eprintln('Accept failed with error code: ${C.errno}')
 			return
 		}
-		set_blocking(client_fd, false) // Set client socket to non-blocking
 
-		// lock_flag.lock()
-		if add_fd_to_epoll(epoll_fd, client_fd, u32(C.EPOLLIN | C.EPOLLET)) == -1 {
-			C.close(client_fd)
+		// Set the client socket to non-blocking mode if accepted successfully
+		set_blocking(client_fd, false)
+
+		// Protect epoll operations with mutex
+		lock {
+			if add_fd_to_epoll(epoll_fd, client_fd, u32(C.EPOLLIN | C.EPOLLET)) == -1 {
+				C.close(client_fd) // Close client socket if adding to epoll fails
+			}
 		}
-		// lock_flag.unlock()
 	}
 }
 
 
-pub fn error_code() int {
-	return C.errno
-}
-
 // Function to handle client closure
-fn handle_client_closure(client_fd int, epoll_fd int,lock_flag &sync.Mutex) {
+fn handle_client_closure(client_fd int, epoll_fd int, lock_flag &sync.Mutex) {
 	// lock_flag.lock()
 	remove_fd_from_epoll(epoll_fd, client_fd)
 	// lock_flag.unlock()
@@ -210,7 +210,7 @@ fn handle_client_closure(client_fd int, epoll_fd int,lock_flag &sync.Mutex) {
 }
 
 // Function to process events from epoll
-fn process_events(epoll_fd int,lock_flag &sync.Mutex) {
+fn process_events(epoll_fd int, lock_flag &sync.Mutex) {
 	events := [max_connection_size]EpollEvent{}
 	num_events := C.epoll_wait(epoll_fd, &events[0], max_connection_size, -1)
 	for i := 0; i < num_events; i++ {
@@ -222,7 +222,8 @@ fn process_events(epoll_fd int,lock_flag &sync.Mutex) {
 		if events[i].events & C.EPOLLIN != 0 {
 			mut buffer := [buffer_size]u8{}
 
-			bytes_read := C.recv(unsafe { events[i].data.fd }, &buffer[0], sizeof(buffer) - 1, 0)
+			bytes_read := C.recv(unsafe { events[i].data.fd }, &buffer[0], sizeof(buffer) - 1,
+				0)
 
 			if bytes_read > 0 {
 				// Send the response back to the client
@@ -231,7 +232,7 @@ fn process_events(epoll_fd int,lock_flag &sync.Mutex) {
 				C.send(unsafe { events[i].data.fd }, response.str, response.len, 0)
 				handle_client_closure(unsafe { events[i].data.fd }, epoll_fd, lock_flag)
 			} else if bytes_read == 0
-				|| (bytes_read < 0 && (error_code()) != 11 && (error_code()) != 11) {
+				|| (bytes_read < 0 && C.errno != C.EAGAIN && C.errno != C.EWOULDBLOCK) {
 				handle_client_closure(unsafe { events[i].data.fd }, epoll_fd, lock_flag)
 			}
 		}
@@ -239,20 +240,20 @@ fn process_events(epoll_fd int,lock_flag &sync.Mutex) {
 }
 
 // Worker thread function
-fn worker_thread(epoll_fd int,lock_flag &sync.Mutex) {
+fn worker_thread(epoll_fd int, lock_flag &sync.Mutex) {
 	for {
 		process_events(epoll_fd, lock_flag)
 	}
 }
 
 // Event loop for accepting clients
-fn event_loop(server_fd int, epoll_fd int,lock_flag &sync.Mutex) {
+fn event_loop(server_fd int, epoll_fd int, lock_flag &sync.Mutex) {
 	for {
 		handle_accept(server_fd, epoll_fd, lock_flag)
 	}
 }
 
-fn cleanup( mut server Server) {
+fn cleanup(mut server Server) {
 	println('Terminating...\n')
 	for i := 0; i < server.thread_pool_size; i++ {
 		// pthread_cancel(threads[i])
@@ -268,7 +269,6 @@ fn cleanup( mut server Server) {
 }
 
 fn main() {
-
 	mut server := Server{}
 	os.signal_opt(.int, fn [mut server] (signum os.Signal) {
 		cleanup(mut server)
